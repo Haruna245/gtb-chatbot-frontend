6376b30ee377d1fa713a16d3396fc77a86759074
unknown <harunaseidujnr@gmail.com>
2023-11-17 21:17:47 +0000

    updated code with some on the map

--- screens/map.js
+++ screens/map.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker, Polyline } from 'react-native-maps';
-import { StyleSheet, View, Button, Alert } from 'react-native';
-//import Geolocation from 'react-native-geolocation-service';
+import { StyleSheet, View, Button, Alert, Text, TextInput, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import * as Location from 'expo-location';
 
-export default function App() {
+export default function App({ navigation }) {
   const [initialRegion, setInitialRegion] = useState({
     latitude: 5.66739, // Replace with your destination latitude 5.55602
     longitude: -0.19153, // Replace with your destination longitude -0.1969
@@ -12,11 +13,55 @@ export default function App() {
   });
   const [userLocation, setUserLocation] = useState(null);
   const [directions, setDirections] = useState([]);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-  
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+      setLocation(location);
+    })();
+  }, []);
+
+  useEffect(() => {
+    // Calculate directions or use a routing API (e.g., Google Directions API) to get directions
+    // and update the "directions" state with the coordinates of the route.
+    // For simplicity, let's assume the user's location and destination are enough to draw a straight line.
+    if (userLocation && initialRegion) {
+      setDirections([
+        { latitude: userLocation.latitude, longitude: userLocation.longitude },
+        { latitude: initialRegion.latitude, longitude: initialRegion.longitude },
+      ]);
+    }
+  }, [userLocation, initialRegion]);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
     <View style={styles.container}>
+      <View style={{ marginTop: 50, margin: 10, width: '100%', flexDirection: 'row' }}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+          <AntDesign name="left" size={24} color="black" />
+        </TouchableOpacity>
+        {/* <View style={{ alignItems: 'center', justifyContent: 'center', width: '60%', marginTop: 20 }}>
+          <Text style={{ fontSize: 20, color: '#FD8936', marginStart: 10 }}>{text}</Text>
+        </View> */}
+      </View>
       <MapView style={styles.map} initialRegion={initialRegion}>
         {userLocation && (
           <Marker coordinate={userLocation} title="Your Location" pinColor="blue" />
@@ -28,9 +73,16 @@ export default function App() {
           }}
           title="Destination"
         />
-        
+        {directions.length > 0 && (
+          <Polyline
+            coordinates={directions}
+            strokeColor="#4CAF50" // Green line color
+            strokeWidth={4} // Line width
+            lineCap="round" // Line cap style
+            lineDashPattern={[10, 5]} // Dashed pattern [line length, gap length]
+          />
+        )}
       </MapView>
-      
     </View>
   );
 }
